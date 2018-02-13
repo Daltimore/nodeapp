705b62d0c38ab4f283bf2c5a5c8308a5fa485390
Daltimore <obinnaleo78@gmail.com>
2018-02-13 21:32:13 +0100

    new jade file

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+var routes = require('./routes');
 
 var app = express();
 
@@ -15,58 +16,44 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
+app.use('/', routes);
 app.use('/users', users);
 
-// catch 404 and forward to error handler
+// catch 404 and forwarding to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
-///error handlers
+/// error handlers
 
-//development error handler
+// development error handler
 // will print stacktrace
-
-if(app.get('env') === 'development'){
-	app.use(function(err, req, res, next){
-		res.status(err.status || 500);
-		res.sender('error', {
-			message:err.message,
-			error: {}
-		});
-	});
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
 }
 
 // production error handler
 // no stacktraces leaked to user
-
-app.use(function(err, req, res, next){
-	res.status('err.status || 500');
-	res.render('error', {
-		message: err.mesage,
-		error: {}
-	});
-});
-
-// error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
 });
 
 module.exports = app;
